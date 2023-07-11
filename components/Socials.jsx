f0b73dae409d9589b1fab0bf707f48a7a035c60b
r2pen2 <joedobbelaar@gmail.com>
2023-07-10 22:18:51 -0400

    Socials.jsx doc + color support

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -13,6 +13,10 @@ const defaultIconSize = "40px";
 /** Whether to always color social icons */
 const coloredIcons = true;
 
+/**
+ * @enum {string} 
+ * Brand hex colors for all supported socials 
+ */
 const brandColors = {
   facebook: "#1877F2",
   instagram: "#E4405F",
@@ -20,6 +24,10 @@ const brandColors = {
   youtube: "#FF0000",
 }
 
+/**
+ * @enum {string} 
+ * Svg title strings for all supported socials 
+ */
 const brandTitles = {
   facebook: "Facebook",
   instagram: "Instagram",
@@ -27,6 +35,10 @@ const brandTitles = {
   youtube: "Youtube",
 }
 
+/**
+ * @enum {string} 
+ * Svg path strings for all supported socials 
+ */
 const brandSvgPaths = {
   facebook: "M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z",
   instagram: "M12 0C8.74 0 8.333.015 7.053.072 5.775.132 4.905.333 4.14.63c-.789.306-1.459.717-2.126 1.384S.935 3.35.63 4.14C.333 4.905.131 5.775.072 7.053.012 8.333 0 8.74 0 12s.015 3.667.072 4.947c.06 1.277.261 2.148.558 2.913.306.788.717 1.459 1.384 2.126.667.666 1.336 1.079 2.126 1.384.766.296 1.636.499 2.913.558C8.333 23.988 8.74 24 12 24s3.667-.015 4.947-.072c1.277-.06 2.148-.262 2.913-.558.788-.306 1.459-.718 2.126-1.384.666-.667 1.079-1.335 1.384-2.126.296-.765.499-1.636.558-2.913.06-1.28.072-1.687.072-4.947s-.015-3.667-.072-4.947c-.06-1.277-.262-2.149-.558-2.913-.306-.789-.718-1.459-1.384-2.126C21.319 1.347 20.651.935 19.86.63c-.765-.297-1.636-.499-2.913-.558C15.667.012 15.26 0 12 0zm0 2.16c3.203 0 3.585.016 4.85.071 1.17.055 1.805.249 2.227.415.562.217.96.477 1.382.896.419.42.679.819.896 1.381.164.422.36 1.057.413 2.227.057 1.266.07 1.646.07 4.85s-.015 3.585-.074 4.85c-.061 1.17-.256 1.805-.421 2.227-.224.562-.479.96-.899 1.382-.419.419-.824.679-1.38.896-.42.164-1.065.36-2.235.413-1.274.057-1.649.07-4.859.07-3.211 0-3.586-.015-4.859-.074-1.171-.061-1.816-.256-2.236-.421-.569-.224-.96-.479-1.379-.899-.421-.419-.69-.824-.9-1.38-.165-.42-.359-1.065-.42-2.235-.045-1.26-.061-1.649-.061-4.844 0-3.196.016-3.586.061-4.861.061-1.17.255-1.814.42-2.234.21-.57.479-.96.9-1.381.419-.419.81-.689 1.379-.898.42-.166 1.051-.361 2.221-.421 1.275-.045 1.65-.06 4.859-.06l.045.03zm0 3.678c-3.405 0-6.162 2.76-6.162 6.162 0 3.405 2.76 6.162 6.162 6.162 3.405 0 6.162-2.76 6.162-6.162 0-3.405-2.76-6.162-6.162-6.162zM12 16c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4zm7.846-10.405c0 .795-.646 1.44-1.44 1.44-.795 0-1.44-.646-1.44-1.44 0-.794.646-1.439 1.44-1.439.793-.001 1.44.645 1.44 1.439z",
@@ -34,10 +46,22 @@ const brandSvgPaths = {
   youtube: "M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z",
 }
 
-function SocialIcon({platformKey, size}) {
+/**
+ * Parent component for all social icons: {@link TikTokIcon}, {@link InstagramIcon}, etc.
+ * @see {@link SocialButton} if you would like to redirect the user on click.
+ * @param {string} params.platformKey - playform key for interfacing with {@link brandColors}, {@link brandTitles}, and {@link brandSvgPaths}
+ * @param {number} params.size - icon size in pixels
+ * @param {string} params.color - SVG fill color override
+ * @default
+ * const defaultIconSize = "40px";
+ */
+function SocialIcon({platformKey, size, color}) {
 
+  /** Title to be displayed on hover from {@link brandTitles} */
   const brandTitle = brandTitles[platformKey];
+  /** SVG fill color from {@link brandColors} */
   const brandColor = brandColors[platformKey];
+  /** SVG path for brand logo from {@link brandSvgPaths} */
   const svgPath = brandSvgPaths[platformKey];
 
   return (
@@ -52,44 +76,100 @@ function SocialIcon({platformKey, size}) {
       }}
     >
       <title>{brandTitle}</title>
-      <path fill={coloredIcons ? brandColor : null} d={svgPath}/>
+      <path fill={color ? color : brandColor} d={svgPath}/>
     </svg>
   );
 }
 
-function YouTubeIcon({size}) {
-  return <SocialIcon platformKey="youtube" size={size}/>
+/**
+ * A Web-Legos custom YouTube icon
+ * @param {number} params.size - icon size in pixels
+ * @returns A {@link SocialIcon} with the YouTube logo
+ */
+function YouTubeIcon({color, size}) { 
+  return <SocialIcon platformKey="youtube" size={size} color={color} />;
 }
 
-function TikTokIcon({size}) {
-  return <SocialIcon platformKey="tiktok" size={size}/>
+/**
+ * A Web-Legos custom TikTok icon
+ * @param {string} params.color - icon fill color override
+ * @param {number} params.size - icon size in pixels
+ * @returns A {@link SocialIcon} with the TikTok logo
+ */
+function TikTokIcon({color, size}) {
+  return <SocialIcon platformKey="tiktok" size={size} color={color} />;
 }
 
-function InstagramIcon({size}) {
-  return <SocialIcon platformKey="instagram" size={size}/>
+/**
+ * A Web-Legos custom Instagram icon
+ * @param {string} params.color - icon fill color override
+ * @param {number} params.size - icon size in pixels
+ * @returns A {@link SocialIcon} with the Instagram logo
+ */
+function InstagramIcon({color, size}) {
+  return <SocialIcon platformKey="instagram" size={size} color={color} />;
 }
 
-function FacebookIcon({size}) {
-  return <SocialIcon platformKey="facebook" size={size}/>
+/**
+ * A Web-Legos custom Facebook icon
+ * @param {string} params.color - icon fill color override
+ * @param {number} params.size - icon size in pixels
+ * @returns A {@link SocialIcon} with the Facebook logo
+ */
+function FacebookIcon({color, size}) {
+  return <SocialIcon platformKey="facebook" size={size} color={color} />;
 }
 
+/**
+ * Parent component for all social buttons: {@link TikTokButton}, {@link InstagramButton}, etc.
+ * @see {@link SocialButton} if you would don't want to allow onClick events
+ * @param {string} params.socialIcon - icon for the button ({@link TikTokIcon}, {@link InstagramIcon}, etc.)
+ * @param {number} params.socialLink - redirect link for onClick event
+ */
 function SocialButton({socialIcon, socialLink}) {
-  console.log(socialIcon);
   return <Button light auto icon={socialIcon} onClick={() => window.open(socialLink, "_blank")} />;
 }
 
-export function YouTubeButton({iconSize, youtubeLink}) {
-  return <SocialButton socialIcon={<YouTubeIcon size={iconSize} />} socialLink={youtubeLink} />;
+/**
+ * A Web-Legos custom YouTube button
+ * @param {string} params.color - icon fill color override
+ * @param {number} params.iconSize - icon size in pixels
+ * @param {string} params.youtubeLink - a link to somewhere on YouTube
+ * @returns A {@link SocialButton} with the YouTube logo
+ */
+export function YouTubeButton({color, iconSize, youtubeLink}) {
+  return <SocialButton socialIcon={<YouTubeIcon size={iconSize} color={color} />} socialLink={youtubeLink} />;
 }
 
-export function TikTokButton({iconSize, tiktokLink}) {
-  return <SocialButton socialIcon={<TikTokIcon size={iconSize} />} socialLink={tiktokLink} />;
+/**
+ * A Web-Legos custom TikTok button
+ * @param {string} params.color - icon fill color override
+ * @param {number} params.iconSize - icon size in pixels
+ * @param {string} params.tikTokLink - a link to somewhere on TikTok
+ * @returns A {@link SocialButton} with the TikTok logo
+ */
+export function TikTokButton({color, iconSize, tiktokLink}) {
+  return <SocialButton socialIcon={<TikTokIcon size={iconSize} color={color} />} socialLink={tiktokLink} />;
 }
 
-export function InstagramButton({iconSize, instagramLink}) {
-  return <SocialButton socialIcon={<InstagramIcon size={iconSize} />} socialLink={instagramLink} />;
+/**
+ * A Web-Legos custom Instagram button
+ * @param {string} params.color - icon fill color override
+ * @param {number} params.iconSize - icon size in pixels
+ * @param {string} params.instagramLink - a link to somewhere on Instagram
+ * @returns A {@link SocialButton} with the Instagram logo
+ */
+export function InstagramButton({color, iconSize, instagramLink}) {
+  return <SocialButton socialIcon={<InstagramIcon size={iconSize} color={color} />} socialLink={instagramLink} />;
 }
 
-export function FacebookButton({iconSize, facebookLink}) {
-  return <SocialButton socialIcon={<FacebookIcon size={iconSize} />} socialLink={facebookLink} />;
+/**
+ * A Web-Legos custom Facebook button
+ * @param {string} params.color - icon fill color override
+ * @param {number} params.iconSize - icon size in pixels
+ * @param {string} params.facebookLink - a link to somewhere on Facebook
+ * @returns A {@link SocialButton} with the Facebook logo
+ */
+export function FacebookButton({color, iconSize, facebookLink}) {
+  return <SocialButton socialIcon={<FacebookIcon size={iconSize} color={color} />} socialLink={facebookLink} />;
 }
