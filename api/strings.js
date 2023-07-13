c9fa5fbd576eed522616ff4f4238ca919e3f1d72
r2pen2 <joedobbelaar@gmail.com>
2023-07-13 17:30:03 -0400

    markdown links!!!

--- api/strings.js
+++ api/strings.js
@@ -1,9 +1,6 @@
 function addClosingTags(htmlString) {
-  
   let lastOpener = -1;
-
   let resultHtml = htmlString;
-
   for (let i in resultHtml) {
     if (resultHtml[i] === '>' && resultHtml[i - 1] !== '/') {
       lastOpener = i;
@@ -13,15 +10,9 @@ function addClosingTags(htmlString) {
       resultHtml = resultHtml.substring(0, i) + "/" + resultHtml.substring(i);
     }
   }
-
+  // Replace line breaks that may have been changed by accident
   resultHtml = resultHtml.replaceAll("</br/>", "<br/>");
-
   return resultHtml;
-}
-
-
-function replaceMultiples(str, regex, replacement) {
-  return str.replace(regex, replacement);
 }
 
 export function HTMLToMarkdown(string) {
@@ -31,15 +22,57 @@ export function HTMLToMarkdown(string) {
   // Replace bolds
   newString = newString.replaceAll("<strong>", "**");
   newString = newString.replaceAll("</strong>", "**");
+
+  // Handle links
+  while (newString.includes("<Link") && newString.includes("</Link")) {
+    
+    const linkStart = newString.indexOf("<Link");
+    const linkEnd = newString.indexOf("</Link>");
+
+    const nameStart = newString.indexOf(">", linkStart);
+    const nameEnd = newString.indexOf("<", nameStart);
+    const nameText = newString.substring(nameStart + 1, nameEnd);
+
+    const hrefStart = newString.indexOf(`href="`, linkStart);
+    const hrefEnd = newString.indexOf(`"`, hrefStart + `href="`.length);
+    const href = newString.substring(hrefStart + `href="`.length, hrefEnd);
+    
+    const markdownString = `[${nameText}](${href})`;
+
+    newString = newString.substring(0, linkStart) + markdownString + newString.substring(linkEnd + "</Link>".length);
+  }
+
   return newString;
 }
 
-export function markdownToHTML(string) {
-  let newString = string;
+/**
+ * Turn any markdown string into HTML for injection
+ * @param {string} markdownString - markdown formatted string
+ * @returns 
+ */
+export function markdownToHTML(markdownString) {
+  let newString = markdownString;
   // Repalce line breaks
-  newString = replaceMultiples(newString, /\n+/g, "<br/>");
+  newString = newString.replace(/\n+/g, "<br/>");
   // Replace bolds
   newString = newString.replaceAll("**", "<strong>");
   newString = addClosingTags(newString);
+
+  // Handle links
+  while (newString.includes("[") && newString.includes("]") && newString.includes("(") && newString.includes(")")) {
+    const nameStart = newString.indexOf("[");
+    const nameEnd = newString.indexOf("]", nameStart);
+    const nameText = newString.substring(nameStart + 1, nameEnd);
+    const hrefStart = newString.indexOf("(");
+    const hrefEnd = newString.indexOf(")", hrefStart);
+    const href = newString.substring(hrefStart + 1, hrefEnd);
+    
+    const isExternal = href.includes(window.location.hostname) ? "isExternal" : ""; 
+
+    const linkElementString = `<Link ${isExternal} href="${href}" target="blank">${nameText}</Link>`;
+
+    newString = newString.substring(0, nameStart) + linkElementString + newString.substring(hrefEnd + 1);
+  }
+
   return newString;
 }
