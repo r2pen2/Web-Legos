2c45b212d3a481ca0fe79f13c7aa991f1b0a4dc4
r2pen2 <joedobbelaar@gmail.com>
2023-07-13 21:25:18 -0400

    SiteImages!

--- components/Images.jsx
+++ components/Images.jsx
@@ -1,6 +1,12 @@
 // Library Imports
 import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
 import { Card, Text } from "@nextui-org/react";
+import { getFileNameByCurrentTime, openFileBrowser } from '../api/files';
+import { useEffect, useState } from 'react';
+import { ImageCompressor } from '../api/images';
+
+// Style imports
+import "../assets/style/images.css";
 
 /**
  * A card component that prompts a user to upload an image
@@ -25,5 +31,64 @@ export function UploadImageCard({onClick, size}) {
         <AddAPhotoIcon sx={{fontSize: iconSize}}/>
       </Card.Body>
     </Card>
+  )
+}
+
+/**
+ * 
+ * @param {string} firestoreId
+ * @param {boolean} editable 
+ * @returns 
+ */
+export function WLImage(props) {
+
+  function uploadImage() {
+    openFileBrowser().then(async (img) => {
+      if (img) {
+        const fileName = getFileNameByCurrentTime(img);
+        const compressedImage = await ImageCompressor.compressImage(img);
+        const formData = new FormData();
+        formData.append("file", compressedImage);
+        formData.append("firestoreId", props.firestoreId);
+        formData.append("fileName", fileName);
+        formData.append("oldFileName", imageFileName);
+
+
+        fetch(`/site-images`, {
+          method: "POST",
+          body: formData,
+        }).then((response) => {
+          if (response.status === 200) {
+            window.location.reload();
+          }
+        });
+      }
+    });
+  }
+
+  const [imageSource, setImageSource] = useState(null);
+  const [imageFileName, setImageFileName] = useState(null);
+
+  useEffect(() => {
+    fetch(`/site-images?id=${props.firestoreId}`).then((res) => {
+      res.json().then(json => {
+        setImageSource(json.source);
+        setImageFileName(json.fileName);
+      })
+    })
+  })
+
+  return (
+    <img 
+      src={imageSource} 
+      alt={props.firestoreId} 
+      className={`img-shadow ${props.editable ? "web-legos-image-edit" : ""}`}
+      onClick={() => {
+        if (props.editable) {
+          uploadImage()
+        }
+      }}
+      style={props.imgCss}
+    />
   )
 }
