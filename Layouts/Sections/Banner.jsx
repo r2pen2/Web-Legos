19b89e2df6bb1cc64135798b84b61050bc460c10
r2pen2 <joedobbelaar@gmail.com>
2023-10-02 16:23:26 -0400

    bannercentered

--- Layouts/Sections/Banner.jsx
+++ Layouts/Sections/Banner.jsx
@@ -4,9 +4,9 @@ import "./banner.css"
 
 class Banner extends Component {
 
-  static Header = (headerProps) => <div data-testid="wl-banner-centered-header" className="wl-text-primary mw-1000">{headerProps.children}</div>;
+  static Header = (headerProps) => <div data-testid="wl-banner-header" className={"wl-text-primary "}>{headerProps.children}</div>;
 
-  static Body = (bodyProps) => <div data-testid="wl-banner-centered-body" className="wl-text-secondary mw-1000">{bodyProps.children}</div>;
+  static Body = (bodyProps) => <div data-testid="wl-banner-body" className={"wl-text-secondary "}>{bodyProps.children}</div>;
 
   header = React.Children.toArray(this.props.children).find(child => child.type === Banner.Header);
   body = React.Children.toArray(this.props.children).find(child => child.type === Banner.Body);
@@ -15,9 +15,28 @@ class Banner extends Component {
 export class BannerCentered extends Banner {
   render() {
     return (
-      <div data-testid="wl-banner-centered" className={`wl-banner flex-column align-items-center justify-content-center ${this.props.dark && "wl-section-dark"}`}>
+      <div data-testid="wl-banner-centered" className={`mw-800 wl-banner flex-column align-items-center justify-content-center ${this.props.dark && "wl-section-dark"}`}>
         {this.header}
         {this.body}
+      </div>
+    )
+  }
+}
+
+export class BannerWithImage extends Banner {
+
+  static ImageContainer = (imageContainerProps) => <div data-testid="wl-banner-with-image-image-container" className="wl-banner-with-image-image-container">{imageContainerProps.children}</div>
+
+  image = React.Children.toArray(this.props.children).find(child => child.type === BannerWithImage.ImageContainer);
+
+  render() {
+    return (
+      <div data-testid="wl-banner-with-image" className={`wl-banner flex-row align-items-center justify-content-center gap-2 ${this.props.dark && "wl-section-dark"}`}>
+        <div className="d-flex flex-column align-items-start justify-content-start mw-650 text-left">
+          {this.header}
+          {this.body}
+        </div>
+        {this.image}
       </div>
     )
   }
--- src/__tests__/layouts/Banners.test.jsx
+++ src/__tests__/layouts/Banners.test.jsx
@@ -1,9 +1,9 @@
 import { fireEvent, render, screen } from "@testing-library/react"
 import "@testing-library/jest-dom"
 import { WLLayoutHeader, WLLayoutText } from "../../../components/Text";
-import { BannerCentered } from "../../../Layouts/Sections/Banner";
+import { BannerCentered, BannerWithImage } from "../../../Layouts/Sections/Banner";
 
-describe("FooterCentered tests", () => {
+describe("BannerCentered tests", () => {
 
   const TestBanner = (props) => (
     <BannerCentered>
@@ -26,13 +26,58 @@ describe("FooterCentered tests", () => {
   
   test("BannerCentered renders header", () => {
     render(<TestBanner />)
-    const header = screen.getByTestId("wl-banner-centered-header");
+    const header = screen.getByTestId("wl-banner-header");
     expect(header).toBeVisible();
   })
   
   test("BannerCentered renders body", () => {
     render(<TestBanner />)
-    const body = screen.getByTestId("wl-banner-centered-body");
+    const body = screen.getByTestId("wl-banner-body");
     expect(body).toBeVisible();
+  })
+})
+
+describe("BannerWithImage tests", () => {
+
+  const TestBanner = (props) => (
+    <BannerWithImage>
+      <BannerWithImage.Header>
+        <WLLayoutHeader editable>Test Header</WLLayoutHeader>
+      </BannerWithImage.Header>
+      <BannerWithImage.Body>
+        <WLLayoutText editable>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias vero accusantium ea nulla nisi unde sunt quasi rem. Quas at, distinctio deserunt voluptatum totam enim! Laudantium nulla natus tempore necessitatibus.
+        </WLLayoutText>
+      </BannerWithImage.Body>
+      <BannerWithImage.ImageContainer>
+        <img data-testid="test-img" src="https://ih1.redbubble.net/image.2563931895.3731/flat,750x1000,075,f.jpg" alt="test-img" />
+      </BannerWithImage.ImageContainer>
+    </BannerWithImage>
+  )
+
+  test("BannerWithImage renders", () => {
+    render(<TestBanner />)
+    const banner = screen.getByTestId("wl-banner-with-image");
+    expect(banner).toBeVisible();
+  })
+  
+  test("BannerWithImage renders header", () => {
+    render(<TestBanner />)
+    const header = screen.getByTestId("wl-banner-header");
+    expect(header).toBeVisible();
+  })
+  
+  test("BannerWithImage renders body", () => {
+    render(<TestBanner />)
+    const body = screen.getByTestId("wl-banner-body");
+    expect(body).toBeVisible();
+  })
+  
+  test("BannerWithImage renders image", () => {
+    render(<TestBanner />)
+    const imageContainer = screen.getByTestId("wl-banner-with-image-image-container");
+    expect(imageContainer).toBeVisible();
+    const image = screen.getByTestId("test-img");
+    expect(image).toBeVisible();
   })
 })
